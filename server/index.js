ee3043237019db8bc0e06217869c19974d22f860
Nick Saxton <nick.saxton@gmail.com>
2020-10-25 10:18:42 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'));
+})
+
+const port = process.env.PORT || 5000;
+
+http.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
