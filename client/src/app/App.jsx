9afba7d51b3d1dec2e66591290477075cc058a10
Nick Saxton <nick.saxton@gmail.com>
2020-10-25 16:15:12 -0400

    Added routing and set up base router

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,9 +1,52 @@
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import LandingPage from './LandingPage';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+}));
+
 const App = () => {
-  return <LandingPage />;
+  const classes = useStyles();
+
+  return (
+    <Grid
+      alignItems="center"
+      className={classes.root}
+      container
+      justify="center"
+      spacing={2}
+    >
+      <Grid item lg={4} md={5} sm={7} xs={12}>
+        <Paper className={classes.paper} elevation={3} padding={2}>
+          <Typography align="center" gutterBottom variant="h3">
+            Scrum Points
+          </Typography>
+          <Router>
+            <Switch>
+              <Route path="/session/create"></Route>
+              <Route path="/session/join"></Route>
+              <Route path="/session/:sessionCode"></Route>
+              <Route path="/">
+                <LandingPage />
+              </Route>
+            </Switch>
+          </Router>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
--- client/src/app/LandingPage.jsx
+++ client/src/app/LandingPage.jsx
@@ -1,57 +1,24 @@
-import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
 import Buttons from '../components/Buttons';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-    padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(2),
-  },
-}));
-
 const LandingPage = () => {
-  const classes = useStyles();
-
   return (
-    <Grid
-      alignItems="center"
-      className={classes.root}
-      container
-      justify="center"
-      spacing={2}
-    >
-      <Grid item lg={4} md={5} sm={7} xs={12}>
-        <Paper className={classes.paper} elevation={3} padding={2}>
-          <Typography align="center" gutterBottom variant="h3">
-            Scrum Points
-          </Typography>
-          <Typography align="center" gutterBottom>
-            Start by creating or joining a session.
-          </Typography>
-          <Buttons>
-            <Button
-              className={classes.button}
-              color="primary"
-              size="large"
-              variant="contained"
-            >
-              Create
-            </Button>
-            <Button className={classes.button} size="large" variant="contained">
-              Join
-            </Button>
-          </Buttons>
-        </Paper>
-      </Grid>
-    </Grid>
+    <>
+      <Typography align="center" gutterBottom>
+        Start by creating or joining a session.
+      </Typography>
+      <Buttons>
+        <Button color="primary" size="large" variant="contained">
+          Create
+        </Button>
+        <Button size="large" variant="contained">
+          Join
+        </Button>
+      </Buttons>
+    </>
   );
 };
 
