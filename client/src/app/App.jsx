1cf1c7573df799adc93ddfe0b05fac5d2317f025
Nick Saxton <nick.saxton@gmail.com>
2020-10-25 15:08:11 -0400

    Set up landing page and styling

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,63 @@
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  buttons: {
+    marginTop: theme.spacing(3),
+    textAlign: 'center',
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <Grid
+      alignItems="center"
+      className={classes.root}
+      container
+      justify="center"
+      spacing={2}
+    >
+      <Grid item xs={12} lg={4}>
+        <Paper className={classes.paper} elevation={3} padding={2}>
+          <Typography align="center" gutterBottom variant="h3">
+            Scrum Points
+          </Typography>
+          <Typography align="center" gutterBottom>
+            Start by creating or joining a session.
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              className={classes.button}
+              color="primary"
+              size="large"
+              variant="contained"
+            >
+              Create
+            </Button>
+            <Button className={classes.button} size="large" variant="contained">
+              Join
+            </Button>
+          </div>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,32 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import blue from '@material-ui/core/colors/blue';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './app/App';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          background: 'linear-gradient(120deg, #89f7fe 0%, #66a6ff 100%);',
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
